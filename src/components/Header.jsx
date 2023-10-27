ab36f06fc8ffc654b65ae3e891b30176a0779d28
kittymiton <kitty.miton@gmail.com>
2023-10-27 16:06:53 +0900

    Fragmentを外す修正

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,13 @@ import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <>
-      <header>
-        <nav>
-          <ul>
-            <li><Link to="/">Blog</Link></li>
-            <li><Link to="/contact">お問い合わせ</Link></li>
-          </ul>
-        </nav>
-      </header>
-    </>
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Blog</Link></li>
+          <li><Link to="/contact">お問い合わせ</Link></li>
+        </ul>
+      </nav>
+    </header>
   )
 };
