7991b367fd26d3187b455424421b5a1b8340fc6c
kittymiton <kitty.miton@gmail.com>
2023-10-30 21:14:14 +0900

    課題1−4

--- src/components/ArticleDetail.jsx
+++ src/components/ArticleDetail.jsx
@@ -21,25 +21,25 @@ export const ArticleDetail = () => {
       .then(data => setPost(data.post))
   }, [id])
 
+  if (!post) return <div>データ読み込み中</div>
+
   return (
     <>
       <main>
         <section>
-          {post && (
-            <div className="post-detail">
-              <img src={post.thumbnailUrl} alt={post.title} />
-              <div className="post-info">
-                <p>{formatDate(post.createdAt)}</p>
-                <ul>{post.categories.map((category, index) => (
-                  <li key={index}>{category}
-                  </li>
-                ))}
-                </ul>
-              </div>
-              <h1>{post.title}</h1>
-              <p dangerouslySetInnerHTML={{ __html: post.content }} />
+          <div className="post-detail">
+            <img src={post.thumbnailUrl} alt={post.title} />
+            <div className="post-info">
+              <p>{formatDate(post.createdAt)}</p>
+              <ul>{post.categories.map((category, index) => (
+                <li key={index}>{category}
+                </li>
+              ))}
+              </ul>
             </div>
-          )}
+            <h1>{post.title}</h1>
+            <p dangerouslySetInnerHTML={{ __html: post.content }} />
+          </div>
         </section>
       </main>
     </>
